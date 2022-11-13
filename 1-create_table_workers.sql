246bea7b247498f9bb84dc6cc8ed22253152d0a5
ferosta <31788151+ferosta@users.noreply.github.com>
2022-11-14 00:51:24 +0300

    выполнено задание 2.3 SQL занятие 1

--- 1-create_table_workers.sql
+++ 1-create_table_workers.sql
@@ -8,21 +8,22 @@
 --  наличие/отсутствие прав(True/False). 
 --  При этом в таблице обязательно должен быть уникальный номер для каждого сотрудника.
 
-CREATE TYPE T_Stage AS ENUM ('jun', 'middle', 'senior', 'lead');
+CREATE TYPE IF NOT EXISTS T_Stage AS ENUM ('jun', 'middle', 'senior', 'lead');
 
-CREATE TABLE Workers(
+CREATE TABLE IF NOT EXISTS Workers
+(
 	Id SERIAL PRIMARY KEY,
-	FIO CHARACTER VARYING (150) NOT NULL,	
+	FIO CHARACTER VARYING (150) NOT NULL UNIQUE,	
 	Begint_Data DATE NOT NULL,
 	Post CHARACTER VARYING (20) NOT NULL,
-	Stage t_Stage NOT NULL DEFAULT 'jun',
+	Stage T_Stage NOT NULL DEFAULT 'jun',
 	Salary INT NOT NULL DEFAULT 20000,
 	Division VARCHAR(10) NOT NULL,
 	Drive_License BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 INSERT INTO Workers (
-	FIO,	
+	FIO,
 	Begint_Data,
 	Post,
 	Stage,
@@ -31,6 +32,6 @@ INSERT INTO Workers (
 	Drive_License
 )
 VALUES 
-	('Иван Иванович Иванов', 2022-02-24, 'м.н.с.', 'jun', 30000, 'лаб.№23', TRUE),
-	('Марина Петровна Петрова', 2021-01-01, 'н.с.', 'middle', 40000, 'лаб.№27', FALSE),
-	('Василий Васильевич Васильев', 1995-10-20, 'в.н.с.', 'lead', 70000, 'лаб.№21', TRUE);
+	('Иван Иванович Иванов', '2022-02-24', 'м.н.с.', 'jun', 30000, 'лаб.№23', TRUE),
+	('Марина Петровна Петрова', '2021-01-01', 'н.с.', 'middle', 40000, 'лаб.№27', FALSE),
+	('Василий Васильевич Васильев', '1995-10-20', 'в.н.с.', 'lead', 70000, 'лаб.№21', TRUE);
